1590b40b410c05248baa1936262349e10f3ecb70
Pierre Ernst <pierre.ernst@elastic.co>
2020-03-17 15:39:40 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const message = core.getInput('message');
+    console.log(`-> ${message}`);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
